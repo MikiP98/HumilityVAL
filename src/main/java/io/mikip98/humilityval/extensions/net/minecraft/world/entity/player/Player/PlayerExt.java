59262e3cd834eff80dad17cb18cd709a30ce320b
MikiP98 <pokora.mikolaj@gmail.com>
2026-07-23 19:40:04 +0200

    Finished inital PlayerExt implementation (message sending)

--- src/main/java/io/mikip98/humilityval/extensions/net/minecraft/world/entity/player/Player/PlayerExt.java
+++ src/main/java/io/mikip98/humilityval/extensions/net/minecraft/world/entity/player/Player/PlayerExt.java
@@ -7,10 +7,30 @@ import net.minecraft.world.entity.player.Player;
 
 @Extension
 public class PlayerExt {
-    public static void avlDisplayClientMessage(@This Player player, String message) {
-        player.avlDisplayClientMessage(Component.literal(message));
+    public static void avlSendServerMessage(@This Player player, String message) {
+        avlSendServerMessage(player, Component.literal(message));
     }
-    public static void avlDisplayClientMessage(@This Player player, Component message) {
+    @SuppressWarnings("resource")
+    public static void avlSendServerMessage(@This Player player, Component message) {
+        if (!player.level().isClientSide()) {
+            player.avlSendMessage(message);
+        }
+    }
+
+    public static void avlSendClientMessage(@This Player player, String message) {
+        player.avlSendClientMessage(Component.literal(message));
+    }
+    @SuppressWarnings("resource")
+    public static void avlSendClientMessage(@This Player player, Component message) {
+        if (player.level().isClientSide()) {
+            player.avlSendMessage(message);
+        }
+    }
+
+    public static void avlSendMessage(@This Player player, String message) {
+        player.avlSendMessage(Component.literal(message));
+    }
+    public static void avlSendMessage(@This Player player, Component message) {
         #if MC_VERSION < 260000
         player.displayClientMessage(message, false);
         #else
