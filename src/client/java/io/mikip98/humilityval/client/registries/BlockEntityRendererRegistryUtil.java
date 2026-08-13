89bd61aa84cd26286785a7de0497e0934b36fc39
MikiP98 <pokora.mikolaj@gmail.com>
2026-08-13 12:25:20 +0200

    Added docstrings to 'BlockEntityRendererRegistryUtil' class

--- src/client/java/io/mikip98/humilityval/client/registries/BlockEntityRendererRegistryUtil.java
+++ src/client/java/io/mikip98/humilityval/client/registries/BlockEntityRendererRegistryUtil.java
@@ -6,7 +6,17 @@ import net.minecraft.client.renderer.blockentity.BlockEntityRenderers;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 
+/**
+ * Utility class for registering Block Entity Renderers.
+ */
 public final class BlockEntityRendererRegistryUtil {
+    /**
+     * Registers a Block Entity Renderer provider to a Block Entity Type.
+     * <br> The method signature changes across MC versions to match the required type of 'BlockEntityRendererProvider'.
+     *
+     * <p>  - Below 1.21.11: {@code <T extends BlockEntity> BlockEntityRendererProvider<T>}
+     * <br> - 1.21.11 and above: {@code <T extends BlockEntity, Y extends BlockEntityRenderState> BlockEntityRendererProvider<T, Y>}
+     */
     #if MC_VERSION < 12111
     public static <T extends BlockEntity> void register(BlockEntityType<T> blockEntityType, BlockEntityRendererProvider<T> provider) {
     #else
