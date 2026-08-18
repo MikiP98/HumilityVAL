d945c8bc68bd0574c49f6ecb465a16ef677672e4
MikiP98 <pokora.mikolaj@gmail.com>
2026-08-19 00:07:36 +0200

    Removed GameRules setter backport as the gamerules names chnaged to the mod code is not reduced

--- src/main/java/io/mikip98/humilityval/extensions/net/minecraft/world/level/GameRules/GameRulesExt.java
+++ src/main/java/io/mikip98/humilityval/extensions/net/minecraft/world/level/GameRules/GameRulesExt.java
@@ -1,21 +1,21 @@
 #if MC_VERSION <= 12111
-package io.mikip98.humilityval.extensions.net.minecraft.world.level.GameRules;
-
-import manifold.ext.rt.api.Extension;
-import manifold.ext.rt.api.This;
-import net.minecraft.server.MinecraftServer;
-import net.minecraft.world.level.GameRules;
-import org.jetbrains.annotations.Nullable;
-
-@Extension
-public class GameRulesExt {
-    public static <T extends GameRules.Value<T>> void set(@This GameRules gameRules, GameRules.Key<T> gameRule, T object, @Nullable MinecraftServer minecraftServer) {
-        GameRules.Value<T> virtualGameRule = gameRules.getRule(gameRule);
-        virtualGameRule.setFrom(object, minecraftServer);
-    }
-//    public static <T extends GameRules.Value<T>> T get(@This GameRules gameRules, GameRules.Key<T> gameRule) {
+//package io.mikip98.humilityval.extensions.net.minecraft.world.level.GameRules;
+//
+//import manifold.ext.rt.api.Extension;
+//import manifold.ext.rt.api.This;
+//import net.minecraft.server.MinecraftServer;
+//import net.minecraft.world.level.GameRules;
+//import org.jetbrains.annotations.Nullable;
+//
+//@Extension
+//public class GameRulesExt {
+//    public static <T extends GameRules.Value<T>> void set(@This GameRules gameRules, GameRules.Key<T> gameRule, T object, @Nullable MinecraftServer minecraftServer) {
 //        GameRules.Value<T> virtualGameRule = gameRules.getRule(gameRule);
-//        return virtualGameRule.get();  // Not possible, 'GameRules.Value<T>' does not store the value...
+//        virtualGameRule.setFrom(object, minecraftServer);
 //    }
-}
+////    public static <T extends GameRules.Value<T>> T get(@This GameRules gameRules, GameRules.Key<T> gameRule) {
+////        GameRules.Value<T> virtualGameRule = gameRules.getRule(gameRule);
+////        return virtualGameRule.get();  // Not possible, 'GameRules.Value<T>' does not store the value...
+////    }
+//}
 #endif
